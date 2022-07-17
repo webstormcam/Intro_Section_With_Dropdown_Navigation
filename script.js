fbd74863f0944cc6543fb42ff300c02f5e573185
Cameron Chandler <webstormcam@gmail.com>
2022-07-17 08:30:17 -0400

    Made the background  darken(mobile)
    When the user opens up the mobile menu, the background of the main page darkens

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const ham = document.getElementById('ham')
 const big_X = document.getElementById('big_X')
 const mobile_Menu = document.getElementById('mobile-menu')
+const darken = document.getElementById('darken-background')
 
 
 ham.addEventListener("click", function(){
   mobile_Menu.style.display='flex'
+  darken.style.display='block'
 } );
 
 big_X.addEventListener("click", function(){
     mobile_Menu.style.display='none'
+    darken.style.display='none'
   } );
